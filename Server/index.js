75808ad6886f785e64daa92650f92fa4a046e1ae
chungnguyen7786 <themask7786@gmail.com>
2021-07-14 23:39:37 +0700

    connect server to mongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
- const app = express()
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://chungnguyen:1234abc@mern-learnit.hmiix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      }
+    )
 
- app.get('/', (req, res) => res.send('Hello world!'))
+    console.log(`MongoDB connected`)
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1) //exit with code "1" => reconnect
+  }
+}
 
- const PORT = 5000
+connectDB()
 
- app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
