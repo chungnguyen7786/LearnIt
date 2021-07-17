96f458c17717ec40948ddc3f7c80df6f2bd40854
chungnguyen7786 <themask7786@gmail.com>
2021-07-17 15:04:44 +0700

    GET posts

--- Server/routes/post.js
+++ Server/routes/post.js
@@ -4,10 +4,24 @@ const verifyToken = require('../middleware/auth')
 
 const Post = require('../models/Post')
 
+// @route GET api/posts
+// @desc Get posts
+// @access Private
+router.get('/', verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.userId }).populate('user', [
+      'username',
+    ])
+    res.json({ success: true, posts })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ succsess: false, message: 'Internal server error.' })
+  }
+})
+
 // @route POST api/posts
 // @desc Create post
 // @access Private
-
 router.post('/', verifyToken, async (req, res) => {
   const { title, description, url, status } = req.body
 
