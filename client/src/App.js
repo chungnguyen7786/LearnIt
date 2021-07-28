1b7639d00cbff171d41f822a0d4b1fdc491d8a4d
chungnguyen7786 <themask7786@gmail.com>
2021-07-28 11:27:24 +0700

    add About

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from './components/layout/Landing'
 import Auth from './views/Auth'
 import AuthContextProvider from './contexts/AuthContext'
 import Dashboard from './views/Dashboard'
+import About from './views/About'
 import ProtectedRoute from './components/routing/ProtectedRoute'
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
             render={(props) => <Auth {...props} authRoute='register' />}
           />
           <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+          <ProtectedRoute exact path='/about' component={About} />
         </Switch>
       </Router>
     </AuthContextProvider>
