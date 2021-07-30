55dc95ca79f32238a1322b7325c7f9e862918ec2
chungnguyen7786 <themask7786@gmail.com>
2021-07-30 22:04:03 +0700

    3rd edition

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,8 @@
-export const apiUrl =
-  process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:5000/api'
-    : 'https://learn-it-vn-2021.herokuapp.com/api '
+// export const apiUrl =
+//   process.env.NODE_ENV !== 'production'
+//     ? 'http://localhost:5000/api'
+//     : 'https://learn-it-vn-2021.herokuapp.com/api '
+export const apiUrl = 'https://learn-it-vn-2021.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
